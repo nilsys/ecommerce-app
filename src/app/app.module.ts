8953cc9afc4a80ff9a3629c0e5e94b85ca0959a6
ibne saif <sahilaktar39@gmail.com>
2020-01-13 20:16:37 +0530

    header created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {BsDropdownModule, CollapseModule} from "ngx-bootstrap";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CollapseModule.forRoot(),
+    BsDropdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+import {BsDropdownConfig} from "ngx-bootstrap";
+
+@Component({
+  selector: 'app-header',
+  templateUrl: 'header.component.html',
+  providers: [{ provide: BsDropdownConfig, useValue: { isAnimated: true, autoClose: true } }]
+})
+export class HeaderComponent {
+  isCollapsed = false;
+}
