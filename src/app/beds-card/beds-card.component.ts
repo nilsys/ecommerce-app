ef9ef01be1153ab14a82aac1c730461e31abf9b6
ibne saif <sahilaktar39@gmail.com>
2020-01-21 22:21:06 +0530

    nothing changed

--- src/app/beds-card/beds-card.component.ts
+++ src/app/beds-card/beds-card.component.ts
@@ -5,7 +5,7 @@ import {ItemService} from '../services/item.service';
 @Component({
   selector: 'app-beds-card',
   templateUrl: './beds-card.component.html',
-  styleUrls: ['./beds-card.component.css']
+  styleUrls: ['./beds-card.component.css'],
 })
 export class BedsCardComponent implements OnInit {
   slideConfig = {
